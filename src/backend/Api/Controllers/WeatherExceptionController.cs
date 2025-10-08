bbffa1b4920fca5658da01b9db50b79d7bf22995
okalashnikov <79265352331@yandex.ru>
2025-10-08 11:36:09 +0300

    Test #48: добавлен тест для IExceptionFilter.

---
+++ src/backend/Api/Controllers/WeatherExceptionController.cs
@@ -0,0 +1,19 @@
+﻿using Api.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherExceptionController : ControllerBase
+    {
+        [Route("Get"), HttpGet]
+        public int Get(int id)
+        {
+            if (id < 0)
+                throw new BadRequestException("Значение должно быть больше 0!");
+
+            return id;
+        }
+    }
+}
---
+++ tests/Api.Tests/Weather/WeatherExceptionTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace Api.Tests.Weather
+{
+    public class WeatherExceptionTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public WeatherExceptionTests(WebApplicationFactory<Program> factory)
+            => _client = factory.CreateClient();
+
+        [Fact]
+        public async Task WeatherForecast_Returns_Ok_And_Array()
+        {
+            var resp = await _client.GetAsync("/api/weatherexception/get/1");
+            Assert.Equal(HttpStatusCode.OK, resp.StatusCode);
+
+            resp = await _client.GetAsync("/api/weatherexception/get/-1");
+            Assert.Equal(HttpStatusCode.BadRequest, resp.StatusCode);
+
+            resp = await _client.GetAsync("/api/unknown/get");
+            Assert.Equal(HttpStatusCode.NotFound, resp.StatusCode);
+        }
+    }
+}
