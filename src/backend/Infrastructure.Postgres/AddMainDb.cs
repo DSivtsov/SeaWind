6661074b632252962268b4f827b0b9be43419387
Denis <dsivtsov@yahoo.com>
2025-10-22 04:00:56 +0300

    Chore: Добавлен MainDbContext в проект Infrastructure.Postgres
    - Создана MainDbContextFactory для проведения миграций
    - Создан AddMainDb.cs для использования DI для сервисов MainDbContext через InfrastructureDI
    - Созданы папки внутри Infrastructure.Postgres\Main
      - Repositories для хранения репозиториев для сущностей MainDbContext
      - Mirations для хранения файлов миграций MainDbContext
    - уточнение AddIdentityDb (opt.UseNpgsql должен вызываться один раз)

---
+++ src/backend/Infrastructure.Postgres/AddMainDb.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.Postgres.Main;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Postgres;
+
+internal static class AddMainDb
+{
+    internal static IServiceCollection AddMainDbContext(this IServiceCollection services,
+        IConfiguration cfg, string connectionString)
+    {
+        // DB для MainDb (использует уже существующий connection string из твоей конфигурации)
+        services.AddDbContext<MainDbContext>(opt =>
+                opt.UseNpgsql(connectionString, npg =>
+                {
+                    npg.EnableRetryOnFailure();
+                    npg.MigrationsAssembly(typeof(MainDbContext).Assembly.FullName);
+                    npg.MigrationsHistoryTable("__EFMigrationsHistory", schema: MainDbContext.Schema);
+                })
+            );
+
+        // Репозитории MainDbContext
+        //services.AddScoped<ICourseRepository, CourseRepositoryPostgres>();
+
+        return services;
+    }
+}
