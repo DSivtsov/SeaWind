d3c0e94cab6b381f0db0e53e8c277baa22335e49
Denis <dsivtsov@yahoo.com>
2025-11-22 02:04:39 +0300

    Chore: update doc and update SeederDto
    Обновление документации после "Chore: Fix duplicate PKeys issue and improve seeding pipeline (#94)":
    - использование пресетов сидирования
    - изменение требования к моделям и Dto
      - Переход на описание моделей через public class с обычными get; set;
      - Создание отдельных DTO для сидирования в Infrastructure.Postgres (не использовать Application DTO)
    
    Дополнительно обновление DTO для сущностей Car & Tester from TimeDbContext

--- src/backend/Infrastructure.Postgres/Time/Seeders/CarsSeeder.cs
+++ src/backend/Infrastructure.Postgres/Time/Seeders/CarsSeeder.cs
@@ -1,27 +1,27 @@
-﻿using Application.DtoTime.Car;
-using Application.ModelsTime;
+﻿using Application.ModelsTime;
 using Infrastructure.Postgres.Seeding.Seeders;
 using Infrastructure.Postgres.Seeding.Shared;
+using Infrastructure.Postgres.Time.SeederDto;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Postgres.Time.Seeders;
 
-internal sealed class CarsSeeder : BaseSeeder<TimeDbContext, Car, CarDto>
+internal sealed class CarsSeeder : BaseSeeder<TimeDbContext, Car, CarSeederDto>
 {
     public override SeedOrder Order => SeedOrder.Core;
 
     // Ищем по уникальному ключу (Id) — быстрее и устойчивее для upsert
-    protected override async Task<Car?> TryFindEntity(TimeDbContext db, CarDto recDemo, CancellationToken ct)
+    protected override async Task<Car?> TryFindEntity(TimeDbContext db, CarSeederDto recDemo, CancellationToken ct)
     {
         return await db.Cars.FirstOrDefaultAsync(x => x.Id == recDemo.Id, ct);
     }
 
     // Добавляем запись в БД
-    protected override void AddNewEntity(TimeDbContext dbContext, CarDto recDemo)
+    protected override void AddNewEntity(TimeDbContext dbContext, CarSeederDto recDemo)
         => dbContext.Cars.Add(new Car(recDemo.Id, recDemo.Model, recDemo.Owner, recDemo.RegNumber));
 
     // обновляем только то, что разрешено
-    protected override void UpdateEntity(Car entity, CarDto recDemo)
+    protected override void UpdateEntity(Car entity, CarSeederDto recDemo)
     {
         entity.Model = recDemo.Model;
         entity.Owner = recDemo.Owner;
--- src/backend/Infrastructure.Postgres/Time/Seeders/TesterSeeder.cs
+++ src/backend/Infrastructure.Postgres/Time/Seeders/TesterSeeder.cs
@@ -1,27 +1,27 @@
-﻿using Application.DtoTime.Tester;
-using Application.ModelsTime;
+﻿using Application.ModelsTime;
 using Infrastructure.Postgres.Seeding.Seeders;
 using Infrastructure.Postgres.Seeding.Shared;
+using Infrastructure.Postgres.Time.SeederDto;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Postgres.Time.Seeders;
 
-internal sealed class TesterSeeder : BaseSeeder<TimeDbContext, Tester, TesterDto>
+internal sealed class TesterSeeder : BaseSeeder<TimeDbContext, Tester, TesterSeederDto>
 {
     public override SeedOrder Order => SeedOrder.Base;
 
     // Ищем по уникальному ключу (Id) — быстрее и устойчивее для upsert
-    protected override async Task<Tester?> TryFindEntity(TimeDbContext db, TesterDto recDemo, CancellationToken ct)
+    protected override async Task<Tester?> TryFindEntity(TimeDbContext db, TesterSeederDto recDemo, CancellationToken ct)
     {
         return await db.Testers.FirstOrDefaultAsync(x => x.Id == recDemo.Id, ct);
     }
 
     // Добавляем запись в БД
-    protected override void AddNewEntity(TimeDbContext db, TesterDto recDemo)
+    protected override void AddNewEntity(TimeDbContext db, TesterSeederDto recDemo)
         => db.Testers.Add(new Tester(recDemo.Id, recDemo.Name, recDemo.Age));
 
     // обновляем только то, что разрешено
-    protected override void UpdateEntity(Tester entity, TesterDto recDemo)
+    protected override void UpdateEntity(Tester entity, TesterSeederDto recDemo)
     {
         entity.Name = recDemo.Name;
         entity.Age = recDemo.Age;
