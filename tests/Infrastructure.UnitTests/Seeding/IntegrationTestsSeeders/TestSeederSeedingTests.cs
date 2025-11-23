bdff274722d98d3877c93fc65ce3a9aba28a2831
tatyana-basargina <basargina.t.a@gmail.com>
2025-11-23 22:38:12 +0300

    fix #79: DataModel: Создание таблицы Courses и демо данных

--- tests/Infrastructure.UnitTests/Seeding/IntegrationTestsSeeders/TestSeederSeedingTests.cs
+++ tests/Infrastructure.UnitTests/Seeding/IntegrationTestsSeeders/TestSeederSeedingTests.cs
@@ -1,5 +1,6 @@
 ﻿using Infrastructure.Postgres.Seeding.Shared;
 using Infrastructure.UnitTests.Seeding.IntegrationTestsSeeders.Shared;
+using Infrastructure.UnitTests.Seeding.UnitTests.Shared;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.UnitTests.Seeding.IntegrationTestsSeeders;
