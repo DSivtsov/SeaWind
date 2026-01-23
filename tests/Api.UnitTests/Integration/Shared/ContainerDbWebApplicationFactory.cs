6b70b7a96854bc9e25cfd47a8edeb7f822ef9587
Denis <dsivtsov@yahoo.com>
2026-01-23 09:25:34 +0300

    Рефакторинг инфрастуктуры для интеграционных тестов ContainerDbFixture
    - запуск каждого теста в чистой БД
    - дял каждого теста свой scope MainDBConext
    - создание и подключение TestAuthHandler для возможности запуска тестов
    для API с авторизацией

--- tests/Api.UnitTests/Integration/Shared/ContainerDbWebApplicationFactory.cs
+++ tests/Api.UnitTests/Integration/Shared/ContainerDbWebApplicationFactory.cs
@@ -1,5 +1,10 @@
 ﻿using Api;
+using Api.UnitTests.Integration.Shared;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 public class ContainerDbWebApplicationFactory : WebApplicationFactory<Program>
@@ -20,4 +25,21 @@ public class ContainerDbWebApplicationFactory : WebApplicationFactory<Program>
 
         return base.CreateHost(builder);
     }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services
+                .AddAuthentication()
+                .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(TestAuthHandler.SchemeName, _ => { });
+
+            services.PostConfigure<AuthenticationOptions>(o =>
+            {
+                o.DefaultAuthenticateScheme = TestAuthHandler.SchemeName;
+                o.DefaultChallengeScheme = TestAuthHandler.SchemeName;
+            });
+        });
+    }
+
 }
---
+++ tests/Api.UnitTests/Integration/Shared/TestAuthHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Api.UnitTests.Integration.Shared;
+
+public sealed class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    public const string SchemeName = "TestAuth";
+
+    public TestAuthHandler(
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder)
+        : base(options, logger, encoder)
+    {
+    }
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, "test-user"),
+            new Claim(ClaimTypes.Name, "Integration Test User"),
+            new Claim(ClaimTypes.Role, "Student"), // если где-то есть [Authorize(Roles=...)]
+        };
+
+        var identity = new ClaimsIdentity(claims, SchemeName);
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, SchemeName);
+
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
